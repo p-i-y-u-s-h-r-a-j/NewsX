304de1b5a2deaf9a94334e91841e8d3b319aceea
Piyush Raj <76215313+p-i-y-u-s-h-r-a-j@users.noreply.github.com>
2023-11-01 18:36:27 +0530

    Update CategoryAdapter.kt

--- app/src/main/java/com/shield/newsx/adapters/CategoryAdapter.kt
+++ app/src/main/java/com/shield/newsx/adapters/CategoryAdapter.kt
@@ -1,12 +1,15 @@
 package com.shield.newsx.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.shield.newsx.DetailActivity
+import com.shield.newsx.MainActivity
 import com.shield.newsx.R
 import com.shield.newsx.databinding.CategoryItemListBinding
 import com.shield.newsx.models.ArticlesItem
@@ -18,7 +21,7 @@ class CategoryAdapter(
     private var mainRecyclerView: RecyclerView,
     private var datalist: List<List<ArticlesItem>?>
 ):
-    RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
+    RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>(), NewsAdapter.onClickListener{
     private val selectedItems = mutableSetOf<Int>()
     inner class CategoryViewHolder(view: View): RecyclerView.ViewHolder(view){
         var binding = CategoryItemListBinding.bind(view)
@@ -47,12 +50,34 @@ class CategoryAdapter(
             finalBinding.categoryName.textSize = 23f
         }
         holder.itemView.setOnClickListener {
-            mainRecyclerView.adapter = NewsAdapter(context, datalist[position])
+            val customNewsAdapter = NewsAdapter(context,datalist[position])
+            mainRecyclerView.adapter = customNewsAdapter
+            customNewsAdapter.setOnItemClickListener(this)
+
             selectedItems.clear()
             selectedItems.add(position)
             notifyDataSetChanged()
         }
 
+    }
+
+    override fun onItemClick(position: Int, adapterData: List<String?>) {
+        var intentImageUrl = adapterData[0]
+        var intentTime = adapterData[1]
+        var intentAuthor = adapterData[2]
+        var intentTitle = adapterData[3]
+        var intentContent = adapterData[4]
+        var intentDes = adapterData[5]
+        var intentUrl = adapterData[6]
+        val intent = Intent(context, DetailActivity::class.java)
+        intent.putExtra("author",intentAuthor)
+        intent.putExtra("title", intentTitle)
+        intent.putExtra("imageurl", intentImageUrl)
+        intent.putExtra("time", intentTime)
+        intent.putExtra("content", intentContent)
+        intent.putExtra("des",intentDes)
+        intent.putExtra("url", intentUrl)
+        context.startActivity(intent)
     }
 
 
